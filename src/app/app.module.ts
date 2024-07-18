caa98c4ea2077bb59ceae6070600c4753ce25949
Diego Adrian Alonso Carlos <diego141200aac@gmail.com>
2024-07-17 20:10:45 -0600

    feat: 2024-07-17 initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ServicesModule } from './services/services.module';
+import { PagesModule } from './pages/pages.module';
+import { InterfacesModule } from './interfaces/interfaces.module';
+import { ComponentsModule } from './components/components.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './Angular_material/material.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    ServicesModule,
+    PagesModule,
+    InterfacesModule,
+    ComponentsModule,
+    MaterialModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
